e30dd7dc6dd0dd09a514998c749c9ec30824dd96
Talen Fisher <tqfisher@iastate.edu>
2018-10-20 12:16:27 -0500

    Add functionality to share button

--- app/src/main/java/notetakr/notetakr/EditorActivity.java
+++ app/src/main/java/notetakr/notetakr/EditorActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -29,12 +31,17 @@ public class EditorActivity extends AppCompatActivity {
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
+        TextInputEditText title = (TextInputEditText) findViewById(R.id.title_bar);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(Intent.EXTRA_TITLE, title.getText());
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, tv.getText());
+                startActivity(Intent.createChooser(sharingIntent, getResources().getString(R.string.share)));
             }
         });
     }
