1165efce5705d8f5eb667ac9fb1ef347d590c2a7
Talen Fisher <tqfisher@iastate.edu>
2018-10-20 01:29:20 -0500

    Adjust capture button

--- app/src/main/java/notetakr/notetakr/MainActivity.java
+++ app/src/main/java/notetakr/notetakr/MainActivity.java
@@ -1,14 +1,16 @@
 package notetakr.notetakr;
 
+import android.graphics.Camera;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//    ImageButton capture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
