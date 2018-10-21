8a01bd9680c12f27ae6ca492caea21d31274eea3
Talen Fisher <tqfisher@iastate.edu>
2018-10-21 01:43:51 -0500

    Add Clear History functionality to HistoryActivity

--- app/src/main/java/notetakr/notetakr/HistoryActivity.java
+++ app/src/main/java/notetakr/notetakr/HistoryActivity.java
@@ -1,5 +1,6 @@
 package notetakr.notetakr;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,16 +14,30 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
 import java.util.Scanner;
 
 public class HistoryActivity extends AppCompatActivity {
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        EditText tv = (EditText)findViewById(R.id.history_textbox);
+        
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        File file = new File(getFilesDir(), "noteTakr.txt");
+        try {
+            String content = new Scanner(file).useDelimiter("\\Z").next();
+            tv.setText(content);
+        } catch (Exception e){
+            Alert alert = new Alert(this, e.toString());
+        }
 
         Spinner mySpinner = (Spinner) findViewById(R.id.spinner2);
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(HistoryActivity.this, android.R.layout.simple_list_item_1,
@@ -30,14 +45,29 @@ public class HistoryActivity extends AppCompatActivity {
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
 
-        File file = new File(getFilesDir(), "noteTakr.txt");
-        try {
-            String content = new Scanner(file).useDelimiter("\\Z").next();
-            EditText tv = (EditText)findViewById(R.id.history_textbox);
-            tv.setText(content);
-        } catch (Exception e){
-            Alert alert = new Alert(this, e.toString());
-        }
+        mySpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+             @Override
+             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                switch(selectedItem) {
+                    case "Clear History":
+                        try {
+                            File file = new File(getFilesDir(), "notesTakr.txt");
+                            Writer writer = new FileWriter(file);
+                            writer.write("");
+                            writer.close();
+                            tv.setText("");
+
+                        } catch(Exception e) {
+                            Alert alert = new Alert(context, e.toString());
+                        }
+
+                        break;
+
+                    default: break;
+                }
+             }
+         });
     }
 
 }
