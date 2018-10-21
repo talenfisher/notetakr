57d55f281be9879d924bcdbe06937296947ba885
Talen Fisher <tqfisher@iastate.edu>
2018-10-21 00:35:39 -0500

    Add newlines to beginning of entries

--- app/src/main/java/notetakr/notetakr/EditorActivity.java
+++ app/src/main/java/notetakr/notetakr/EditorActivity.java
@@ -82,7 +82,7 @@ public class EditorActivity extends AppCompatActivity {
         try {
             Writer writer = new FileWriter(file, true);
             Date date = new Date();
-            writer.write("---------------------\nDate: " + date.toString() + " Title: " + title + "\n\n" + s);
+            writer.write("\n\n---------------------\nDate: " + date.toString() + " Title: " + title + "\n\n" + s);
             writer.close();
         } catch (Exception e){
             Alert alert = new Alert(context, e.toString());
