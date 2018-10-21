c1cd6f9210e1a3987c2081064c42d5d865b76472
Talen Fisher <tqfisher@iastate.edu>
2018-10-20 22:14:51 -0500

    Adjustments to saving, remove FloatingActionButton

--- app/src/main/java/notetakr/notetakr/EditorActivity.java
+++ app/src/main/java/notetakr/notetakr/EditorActivity.java
@@ -28,59 +28,57 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
         // Get transcription from previous activity
         Intent intent = getIntent();
         String data = intent.getStringExtra("data");
+
+        EditText tv = (EditText)findViewById(R.id.content_edit);
+        tv.setText(data);
+
+        TextInputEditText title = (TextInputEditText) findViewById(R.id.title_bar);
+
         Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
         mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selectedItem = parent.getItemAtPosition(position).toString();
-                if(selectedItem.equals("Save External")){
-                    writeFile(data);
-                    Alert alert = new Alert(context, getFilesDir().getAbsolutePath()); alert.show();
+
+                switch(selectedItem) {
+                    case "Save Internal":
+                        writeFile(title.getText().toString(), data);
+                        Alert alert = new Alert(context, getFilesDir().getAbsolutePath() + "noteTakr.txt"); alert.show();
+                        break;
+
+                    case "Save External":
+                        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                        sharingIntent.setType("text/plain");
+                        sharingIntent.putExtra(Intent.EXTRA_TITLE, title.getText().toString());
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, tv.getText());
+                        startActivity(Intent.createChooser(sharingIntent, getResources().getString(R.string.share)));
+                        break;
+
+                    default: break;
                 }
             }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
+            @Override public void onNothingSelected(AdapterView<?> parent) {}
         });
 
-        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(EditorActivity.this,android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.Save));
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(EditorActivity.this,android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Save));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
-
-        EditText tv = (EditText)findViewById(R.id.content_edit);
-        tv.setText(data);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
-
-        TextInputEditText title = (TextInputEditText) findViewById(R.id.title_bar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-                sharingIntent.setType("text/plain");
-                sharingIntent.putExtra(Intent.EXTRA_TITLE, title.getText());
-                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, tv.getText());
-                startActivity(Intent.createChooser(sharingIntent, getResources().getString(R.string.share)));
-            }
-        });
     }
-    private void writeFile(String s){
+    private void writeFile(String title, String s){
         File file = new File(getFilesDir(), "noteTakr.txt");
         try {
             Writer writer = new FileWriter(file, true);
             Date date = new Date();
-            writer.write(date.toString() + "\n" + "-------------");
-            writer.write("\n" + s);
+            writer.write("---------------------\nDate: " + date.toString() + " Title: " + title + "\n\n" + s);
             writer.close();
         } catch (Exception e){
             Alert alert = new Alert(context, e.toString());
