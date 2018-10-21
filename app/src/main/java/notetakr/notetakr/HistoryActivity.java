81ed812254e9861b5a8c6deaae83aff06599bdaf
Hung Truong <hungtruong19999@gmail.com>
2018-10-21 00:55:10 -0600

    History Update

--- app/src/main/java/notetakr/notetakr/HistoryActivity.java
+++ app/src/main/java/notetakr/notetakr/HistoryActivity.java
@@ -7,7 +7,10 @@ import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.io.File;
 import java.util.Scanner;
@@ -20,6 +23,12 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(HistoryActivity.this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.History_Options));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
 
         File file = new File(getFilesDir(), "noteTakr.txt");
         try {
